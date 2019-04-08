437a3f8d4187eb824b5207633b00e09ce4132977
Alexey Deboy <adeboy@gomel.iba.by>
2019-04-08 23:48:48 +0300

    Toast now on the top of the screen

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,18 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0,180);
+                toast.show();
             }
+
         });
-        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(MainActivity.this,R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,180);
+                toast.show();
             }
         });
     }
